d8367a7fb5d96ef9d5ffaff535a2f5ba4b972cf0
Yue Sheng <a.yuesheng@gmail.com>
2024-08-03 04:17:04 +0800

    Added Logic
    Added the final pieces of logic for the Calculator
    Allows for negative numbers
    Auto Rounding for decimals
    Shows OVERFLOW if number of digits exceed certain number

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ let allNums = "0123456789."
 let allOperands = "+-*/"
 let storedNumber
 let operand
+let isZero = false
 
 // Create number button listeners
 document.querySelectorAll(".numberBtn").forEach((numberBtn)=>{
@@ -14,42 +15,52 @@ document.querySelectorAll(".numberBtn").forEach((numberBtn)=>{
 })
 
 // Create function button Listeners
-document.querySelector("#clear").addEventListener("click", clearCurr)
+document.querySelector("#clear").addEventListener("click", ()=>{
+    if(!allOperands.includes(history.textContent[-1])) clearAll()
+    else clearCurr})
 document.querySelector("#del").addEventListener("click",delCurrNum)
 document.querySelector("#allClear").addEventListener("click",clearAll)
 
 // Add KeyboardListener
 document.addEventListener(
     "keydown",(e)=>{
+        e.preventDefault()
         if(allNums.includes(e.key)) addToCurrNum(e.key)
         else if(e.key =="Backspace" || e.key=="Delete") delCurrNum()
+        else if (allOperands.includes(e.key)) operate(e.key)
+        else if (e.key =="Enter") equal()
     }       
 )
 
 // Create Operand button Listeneres
 document.querySelectorAll(".operand").forEach((operandBtn)=>{
     operandBtn.addEventListener("click",(e)=>{
-        if (storedNumber!=undefined){
-
-        }
-        if(currNum.textContent!=="0"){
-        storeNum()
-        operand = e.target.value
-        history.textContent = storedNumber + operand 
-        }
+          operate(e.target.value)
     })
 })
+// Equal Listener
+document.querySelector("#equal").addEventListener("click",equal)
 
 // Add Num Logic
 function addToCurrNum(value){
     let limit =13
-    if(currNum.textContent=="0"){
+    if (operand=="="){
+        clearAll()
+        addToCurrNum(value)
+        return
+    }
+    if(currNum.textContent=="0"||currNum.textContent=="-0"){
         if(value==".") {
             currNum.textContent+=value 
             return}
     }
+    else if (currNum.textContent.includes(".") && value ==".") return
     else if(currNum.textContent.includes(".")) limit =14
-    if( currNum.textContent.length<limit) currNum.textContent= (currNum.textContent=="0")?value:currNum.textContent+value
+    if( currNum.textContent.length<limit) {
+        if(currNum.textContent=="0")currNum.textContent= value
+        else if (currNum.textContent=="-0")currNum.textContent="-"+value
+        else currNum.textContent+=value
+    }
 }
 
 // Store Num
@@ -57,6 +68,7 @@ function storeNum(){
     inputNum = currNum.textContent
     if(inputNum[-1]==".") inputNum = inputNum.slice(0,-1)
     storedNumber = inputNum
+    console.log(storedNumber)
     clearCurr()
     
 }
@@ -77,18 +89,69 @@ function clearAll(){
     storedNumber = undefined
     operand = undefined
 }
-function evaluateHistory(){
+
+// Operate
+function operate(input){
+    if(currNum.textContent=="0" && input=="-"){
+        currNum.textContent = "-0"
+        return
+    }
+    else if(currNum.textContent=="-0" && input =="-"){
+        currNum.textContent = "0"
+        return
+    }
+    if (storedNumber!=undefined && operand!="="){
+        let result = evaluate()
+        currNum.textContent = result
+    }
+
+    storeNum()
+    operand = input
+    history.textContent = storedNumber + operand 
+
+}
+// Equal
+function equal(){
+    
+    if(operand=="=" || operand==undefined) return
+    let result = evaluate()
+    history.textContent += currNum.textContent
+    currNum.textContent = result
+    storedNumber = result
+    operand = "="
+}
+
+// evaluate
+function evaluate(){
+    
+    num1 = parseFloat(storedNumber)
+    num2 = parseFloat(currNum.textContent)
+    console.log("Stored: "+storedNumber+" Current: "+currNum.textContent)
+    let result
     switch(operand){
         case "+":
+            history.textContent = storedNumber + operand
+            result= num1 + num2
             break
         case "-":
+            history.textContent = storedNumber + operand
+            result= num1 - num2
             break
         case "/":
+            history.textContent = storedNumber + operand
+            result= num1/num2
             break
         case "*":
+            history.textContent = storedNumber + operand
+            result= num1*num2
             break
-        case "=":
-            break
-            
     }
+    result = result.toString()
+    let size
+    if (result.includes(".")) size = result.indexOf(".")+1
+    else size = result.length
+    if (size>=14) {result= "OVERFLOW"}
+    else if (result.length>=14) result= result.slice(0,14)
+    return result
+    
 }
